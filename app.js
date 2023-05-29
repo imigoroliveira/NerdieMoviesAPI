cdb6fad6a443c26abd4db60281e34d6333e366fe
idamara moraes <igormat4us@gmail.com>
2023-05-29 20:50:28 -0300

    Setting up mongodb and routes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+require('./database/mongodb');
 
 var app = express();
 
@@ -20,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/assinantes', assinantesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
